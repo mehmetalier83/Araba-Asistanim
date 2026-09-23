061c2bcbaa6b07732b29fdee65154ad02bae5c4a
Mehmet Ali Er <mehmetalier83@hotmail.com>
2026-09-23 14:27:18 +0300

    Initial Commit

---
+++ Araba Asistanim/Araba_AsistanimApp.swift
@@ -0,0 +1,17 @@
+//
+//  Araba_AsistanimApp.swift
+//  Araba Asistanim
+//
+//  Created by Mehmet Ali on 23.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct Araba_AsistanimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Araba Asistanim/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Araba Asistanim
+//
+//  Created by Mehmet Ali on 23.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
